6d41459edb7f3603b3881bf157017755f75c222f
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-08-13 19:47:06 +0000

    Finished

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let domainTemplate = [pronoun, adj, noun];
+let ext = [".com", ".net", ".us", ".io", ".cl"];
+let domainTemplate = [pronoun, adj, noun, ext];
 
 const domainGenerator = (obj) => {
   let domainArray = [];
-    for (let i in obj[2]){
-        for (let ii in obj[1]){
-            for (let iii in obj[0]){
-                domainArray.push(`${obj[0][iii]}${obj[1][ii]}${obj[2][i]}.com`);
-            }
+
+  for (let iv in obj[3]) {
+    for (let i in obj[2]) {
+      for (let ii in obj[1]) {
+        for (let iii in obj[0]) {
+          domainArray.push(`${obj[0][iii]}${obj[1][ii]}${obj[2][i]}${obj[3][iv]}`);
         }
+      }
     }
+  }
   return domainArray;
 };
 
